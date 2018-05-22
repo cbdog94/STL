253140cee3931918b833005831642d85ff0f7e3b
Cheng Bin <cbdog94@gmail.com>
2018-05-22 19:35:04 +0800

    Add more city support.

--- src/main/java/FindPair.java
+++ src/main/java/FindPair.java
@@ -1,44 +1,65 @@
 import bean.Cell;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import hbase.TrajectoryUtil;
-import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import util.CommonUtil;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FindPair {
-    public static void main(String[] args) throws IOException {
-        File startFile = FileUtils.getFile("file/start.txt");
-        List<String> startLines = FileUtils.readLines(startFile);
-        Map<String, Integer> startMap = new HashMap<>();
-        for (String line : startLines) {
-            String[] splits = line.split("\t");
-            if (Integer.parseInt(splits[1]) > 1000)
-                startMap.put(splits[0], Integer.parseInt(splits[1]));
-        }
 
-        File endFile = FileUtils.getFile("file/end.txt");
-        List<String> endLines = FileUtils.readLines(endFile);
-        Map<String, Integer> endMap = new HashMap<>();
-        for (String line : endLines) {
-            String[] splits = line.split("\t");
-            if (Integer.parseInt(splits[1]) > 1000)
-                endMap.put(splits[0], Integer.parseInt(splits[1]));
-        }
+    @Parameter(names = {"--city", "-c"}, description = "Which city to be counted (SH/SZ/CD).", required = true, validateWith = CommonUtil.CityValidator.class)
+    private String city;
 
-        for (String startKey : startMap.keySet())
-            for (String endKey : endMap.keySet()) {
-                Cell startCell = new Cell(startKey);
-                Cell endCell = new Cell(endKey);
-                if (Math.abs(startCell.getTileX() - endCell.getTileX()) > 10 && Math.abs(startCell.getTileY() - endCell.getTileY()) > 10) {
-                    Set<String> ids = TrajectoryUtil.getAllTrajectoryID(startCell, endCell,"SH");
-                    if (ids.size() < 150&&ids.size() > 100 )
-                        System.out.println(startCell + " " + endCell + " " + ids.size());
+    private static String hdfs = "hdfs://192.168.0.53:9000";
+
+    public static void main(String... argv) {
+        FindPair main = new FindPair();
+        JCommander.newBuilder()
+                .addObject(main)
+                .build()
+                .parse(argv);
+        try {
+            main.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void run() throws IOException, URISyntaxException {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFs", hdfs);
+        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+        FileSystem fs = FileSystem.get(new URI(hdfs), conf);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(new Path(hdfs + "/" + city.toLowerCase() + "_start_hotspot/part-r-00000"))));
+        List<String> SHStart = br.lines().map(s -> s.split("\t")[0]).collect(Collectors.toList());
+        List<String> SHSUBStart = SHStart.subList(SHStart.size() - 10, SHStart.size());
+
+        br = new BufferedReader(new InputStreamReader(fs.open(new Path(hdfs + "/" + city.toLowerCase() + "_end_hotspot/part-r-00000"))));
+        List<String> SHEnd = br.lines().map(s -> s.split("\t")[0]).collect(Collectors.toList());
+        List<String> SHSubEnd = SHEnd.subList(SHEnd.size() - 10, SHEnd.size());
+
+
+        SHSUBStart.parallelStream().forEach(start -> {
+            SHSubEnd.parallelStream().forEach(end -> {
+                Cell startCell = new Cell(start), endCell = new Cell(end);
+                if (Math.pow(endCell.getTileY() - startCell.getTileY(), 2) + Math.pow(endCell.getTileX() - startCell.getTileX(), 2) > 100) {
+                    int size = TrajectoryUtil.getAllTrajectoryGPSs(startCell, endCell, city).size();
+                    if (size > 100)
+                        System.out.println(start + " " + end + " " + size);
                 }
+            });
+        });
 
-            }
     }
 }
--- src/main/java/bean/GPS.java
+++ src/main/java/bean/GPS.java
@@ -72,8 +72,44 @@ public class GPS implements Serializable {
         return "[" + longitude + "," + latitude + "," + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(timestamp) + "]";
     }
 
-    public GPXEntry convertToGPX() {
+    public GPXEntry convertToGPX(String city) {
+        if (city.equals("CD")) {
+            double[] wgsGPS = gcj2wgs(latitude, longitude);
+            return new GPXEntry(wgsGPS[0], wgsGPS[1], timestamp.getTime());
+
+        }
         return new GPXEntry(latitude, longitude, timestamp.getTime());
+    }
+
+    private double[] gcj2wgs(double lat, double lng) {
+        double a = 6378245.0, ee = 0.00669342162296594323;
+        double dLat = transformLat(lng - 105.0, lat - 35.0);
+        double dLng = transformLon(lng - 105.0, lat - 35.0);
+        double radLat = lat / 180.0 * Math.PI;
+        double magic = Math.sin(radLat);
+        magic = 1 - ee * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * Math.PI);
+        dLng = (dLng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * Math.PI);
+        double mgLat = lat + dLat;
+        double mgLng = lng + dLng;
+        return new double[]{lat * 2 - mgLat, lng * 2 - mgLng};
+    }
+
+    private double transformLat(double lat, double lon) {
+        double ret = -100.0 + 2.0 * lat + 3.0 * lon + 0.2 * lon * lon + 0.1 * lat * lon + 0.2 * Math.sqrt(Math.abs(lat));
+        ret += (20.0 * Math.sin(6.0 * lat * Math.PI) + 20.0 * Math.sin(2.0 * lat * Math.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(lon * Math.PI) + 40.0 * Math.sin(lon / 3.0 * Math.PI)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(lon / 12.0 * Math.PI) + 320 * Math.sin(lon * Math.PI / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    private double transformLon(double lat, double lon) {
+        double ret = 300.0 + lat + 2.0 * lon + 0.1 * lat * lat + 0.1 * lat * lon + 0.1 * Math.sqrt(Math.abs(lat));
+        ret += (20.0 * Math.sin(6.0 * lat * Math.PI) + 20.0 * Math.sin(2.0 * lat * Math.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(lat * Math.PI) + 40.0 * Math.sin(lat / 3.0 * Math.PI)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(lat / 12.0 * Math.PI) + 300.0 * Math.sin(lat / 30.0 * Math.PI)) * 2.0 / 3.0;
+        return ret;
     }
 
 }
